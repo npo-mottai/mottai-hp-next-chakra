38a3d2e2f4046a6cc20a8b4429c2bde78301f8f9
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-11-02 16:25:24 +0900

    feat: add chakra ui configuration related codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+// import '../styles/globals.css'
+// import { ChakraProvider } from '@chakra-ui/react'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps): JSX.Element {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   )
+// }
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
