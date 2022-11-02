9712de0e0f584f0d63d3a5b7ea6acfdbbd5a1b66
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-11-02 16:26:37 +0900

    chore: remove unnecessary codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,3 @@
-// import '../styles/globals.css'
-// import { ChakraProvider } from '@chakra-ui/react'
-// import type { AppProps } from 'next/app'
-
-// export default function App({ Component, pageProps }: AppProps): JSX.Element {
-//   return (
-//     <ChakraProvider>
-//       <Component {...pageProps} />
-//     </ChakraProvider>
-//   )
-// }
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
