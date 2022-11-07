ea017e04de447b74f3f6f4febea10fd1e537b2cd
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-11-07 19:35:28 +0900

    chore: rename layout dierectory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../config/chakra'
-import Layout from '../components/layout/Layout'
+import Layout from '../components/layouts/Layout'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
