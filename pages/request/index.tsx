3a9f63609cf71418320bf50f8994cefae87465ea
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-11-10 21:49:31 +0900

    feat: add donation page

---
+++ pages/request/index.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react'
+import MainVisual from '../../components/MainVisual'
+import SectionContainer from '../../components/top-page/SectionContainer'
+import ContentContainer from '../../components/ContentContainer'
+import RequestSupportMessage from '../../components/support-page/RequestSupportMessage'
+import { HowToSupport } from '../../components/support-page/HowToSupport'
+
+export default function RequestPage() {
+  return (
+    <Box>
+      <MainVisual title={'MOTTAIをサポートする'} />
+      <ContentContainer>
+        <SectionContainer title={'MOTTAIの想い'}>
+          <RequestSupportMessage />
+        </SectionContainer>
+        <SectionContainer title={'MOTTAIをサポートする'}>
+          <HowToSupport />
+        </SectionContainer>
+      </ContentContainer>
+    </Box>
+  )
+}
