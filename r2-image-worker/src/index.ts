0b354152b44cc2977b982a56eacdde83933aa9b2
MK776 <aa070706511@gmail.com>
2024-05-19 11:50:12 +0000

    feat: add r2 image worker

--- r2-image-worker/src/index.ts
+++ r2-image-worker/src/index.ts
@@ -1,18 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+      switch (request.method) {
+        case 'GET':
+          const object = await env.MY_BUCKET.get(key);
+          if (object === null) {
+          return new Response('Object Not Found', { status: 404 });
+          }
+          const headers = new Headers();
+          object.writeHttpMetadata(headers);
+          headers.set('etag', object.httpEtag);
+          return new Response(object.body, {
+          headers
+          });
+        default:
+          return new Response('Method Not Allowed', {
+          status: 405,
+          headers: {
+            Allow: 'GET'
+          }
+          });
+      }
+    }
 };
