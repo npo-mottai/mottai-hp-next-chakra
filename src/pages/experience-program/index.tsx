4e9b0364cd8b8ba91c2801ce5e0f528b8c57e9da
MK776 <aa070706511@gmail.com>
2024-09-14 14:00:04 +0000

    feat: add links 2 experience program page

--- src/pages/experience-program/index.tsx
+++ src/pages/experience-program/index.tsx
@@ -3,8 +3,8 @@ import { Box } from '@chakra-ui/react'
 import ContentContainer from '../../components/ContentContainer'
 import MainVisual from '../../components/MainVisual'
 import TopicPath from '../../components/TopicPath'
+import ActivityList from '../../components/activity/ActivityList'
 import ExperienceProgram from '../../components/experience-program-page/ExperienceProgram'
-import ExperienceProgramList from '../../components/experience-program-page/ExperienceProgramList'
 import SectionContainer from '../../components/top-page/SectionContainer'
 import { getNotionActivityData } from '../../utils/notion'
 
@@ -20,8 +20,21 @@ export default function ExperienceProgramPage({ experienceProgramLinkArr }: Infe
           <SectionContainer id={'experience-program'} title={'体験事業'}>
             <ExperienceProgram />
           </SectionContainer>
+          <SectionContainer id={'news'} title={'開催予定'}>
+            <ActivityList activityLinkArr={experienceProgramLinkArr} />
+          </SectionContainer>
         </ContentContainer>
       </Box>
     </>
   )
 }
+
+// TODO: コメントを追加する
+/** */
+export const getStaticProps: GetStaticProps<{
+  experienceProgramLinkArr: activityLinkObj[]
+}> = (async () => {
+  let experienceProgramLinkArr = await getNotionActivityData('experience-program' as string)
+
+  return { props: { experienceProgramLinkArr } }
+})
